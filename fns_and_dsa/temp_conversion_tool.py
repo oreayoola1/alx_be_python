8b9ea36ca198c3f50c047f8aae451be9e87637ee
oreayoola1 <oreayoola1@gmail.com>
2025-06-14 18:30:30 +0100

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,21 +1,11 @@
-def define_factors():
-    global FAHRENHEIT_TO_CELSIUS_FACTOR
-    global CELSIUS_TO_FAHRENHEIT_FACTOR
-    FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-    CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
-
-define_factors()
+# Global conversion factors
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
-    """
-    Converts Fahrenheit to Celsius using the global conversion factor.
-    """
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    """
-    Converts Celsius to Fahrenheit using the global conversion factor.
-    """
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 def main():
@@ -39,4 +29,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
